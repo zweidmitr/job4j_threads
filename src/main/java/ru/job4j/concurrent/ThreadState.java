855e99d4e3263d42da829c7008edc93e9f0af8d8
Dmitrii Zwei <zweidmitr@gmail.com>
2022-01-31 10:42:27 +0300

    1.1. Состояние нити. [#229175] step2

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -1,34 +1,26 @@
 package ru.job4j.concurrent;
 
-import java.time.LocalTime;
-
 public class ThreadState {
     public static void main(String[] args) {
         Thread first = new Thread(
                 () -> {
                 }
         );
-        System.out.println("Thread : "
-                + first.getName() + " is " + first.getState());
-        first.start();
         Thread second = new Thread(
                 () -> {
                 }
         );
 
+        first.start();
         second.start();
-        System.out.println(LocalTime.now() + " Thread: "
-                + second.getName() + " is " + second.getState());
 
-        while (first.getState() != Thread.State.TERMINATED) {
-            System.out.println("Thread: "
-                    + first.getState() + " is " + first.getState());
+        while (first.getState() != Thread.State.TERMINATED
+                || second.getState() != Thread.State.TERMINATED) {
+            System.out.println("Thread : "
+                    + second.getName() + " is " + second.getState());
+            System.out.println("Thread : "
+                    + first.getName() + " is " + first.getState());
         }
-        System.out.println(Thread.currentThread().getName());
-        System.out.println("Thread: "
-                + first.getName() + " is " + first.getState());
-        System.out.println(LocalTime.now() + " Thread: "
-                + second.getName() + " is " + second.getState());
-
+        System.out.println("работа завершена");
     }
 }
