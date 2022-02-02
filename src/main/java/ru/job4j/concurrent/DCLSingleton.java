9eb47fe25ce5b753e584e3670d8c2bd6b2227c19
Dmitrii Zwei <zweidmitr@gmail.com>
2022-02-02 08:50:45 +0300

    2. Модель памяти Java [#267917]

---
+++ src/main/java/ru/job4j/concurrent/DCLSingleton.java
@@ -0,0 +1,20 @@
+package ru.job4j.concurrent;
+
+public class DCLSingleton {
+    private static volatile DCLSingleton inst;
+
+    public static DCLSingleton instOf() {
+        if (inst == null) {
+            synchronized (DCLSingleton.class) {
+                if (inst == null) {
+                    inst = new DCLSingleton();
+                }
+            }
+        }
+        return inst;
+    }
+
+    private DCLSingleton() {
+
+    }
+}
