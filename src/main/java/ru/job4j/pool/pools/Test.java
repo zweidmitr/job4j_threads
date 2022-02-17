22586664af3146ce60f9a714c7a1282c41ea8809
Dmitrii Zwei <zweidmitr@gmail.com>
2022-02-17 16:40:45 +0300

    task)

--- src/main/java/ru/job4j/pool/pools/Test.java
+++ src/main/java/ru/job4j/pool/pools/Test.java
@@ -4,19 +4,23 @@ import java.util.Random;
 
 public class Test {
     public static void main(String[] args) {
-        int size = 10;
+        int size = 150;
         int[] array = new int[size];
         Random rand = new Random();
         for (int i = 0; i < array.length; i++) {
             array[i] = rand.nextInt(size);
         }
-        int[] newSortArray = MergeSort.sort(array);
-        for (int index = 0; index < array.length; index++) {
-            System.out.println("было: "
-                    + array[index]
-                    + " стало: "
-                    + newSortArray[index]
-                    + "   index [" + index + "]");
+        for (int elem : array) {
+            System.out.print(elem + " ");
         }
+        System.out.println("\n========");
+        int temp = Integer.MAX_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            int el = Math.abs(array[i]);
+            if (el < temp) {
+                temp = array[i];
+            }
+        }
+        System.out.println(temp);
     }
 }
