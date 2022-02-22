8183389da5c7483e692d95c6456a5cd9e5169ebe
Dmitrii Zwei <zweidmitr@gmail.com>
2022-02-22 11:52:11 +0300

    commitTest_all

--- src/main/java/ru/job4j/pool/pools/Test.java
+++ src/main/java/ru/job4j/pool/pools/Test.java
@@ -7,17 +7,11 @@ public class Test {
         int size = 150;
         int[] array = new int[size];
         Random rand = new Random();
-        for (int i = 0; i < array.length; i++) {
-            array[i] = rand.nextInt(size);
+        for (int index = 0; index < array.length; index++) {
+            array[index] = rand.nextInt(size);
         }
         for (int elem : array) {
             System.out.print(elem + " ");
         }
-        System.out.println("\n========");
-        int temp = Integer.MAX_VALUE;
-        for (int index = 0; index < array.length; index++) {
-            int el = Math.abs(array[index]);
-        }
-        System.out.println(temp);
     }
 }
