0e87d05afd85782073c9d0ff41de10c5e3f7cc04
Dmitrii Zwei <zweidmitr@gmail.com>
2022-03-05 20:56:23 +0300

    Откат изменений

--- src/main/java/ru/job4j/pool/pools/Test.java
+++ src/main/java/ru/job4j/pool/pools/Test.java
@@ -4,14 +4,20 @@ import java.util.Random;
 
 public class Test {
     public static void main(String[] args) {
-        int size = 150;
+        int size = 200;
         int[] array = new int[size];
         Random rand = new Random();
         for (int index = 0; index < array.length; index++) {
             array[index] = rand.nextInt(size);
         }
+        int count = 0;
         for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + "  ");
+            System.out.printf("%04d ", array[i]);
+            count++;
+            if (count == 10) {
+                System.out.println();
+                count = 0;
+            }
         }
     }
 }
